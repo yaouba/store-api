7fe60c3940cb8c762d4513946de421b9a012e44d
yaouba <kabayyaouba@gmail.com>
2024-04-29 23:29:18 +0100

    add middleware and connect to DB

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const connectDB = require('./db/connect')
+
+// middlewares
+app.use(express.json())
+
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Store')
+})
+
+// products route
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log('Server listening ...'))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
